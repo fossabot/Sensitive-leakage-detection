5a7867ac7882763612d52ab0b2fb92a7e029d97b
laolao <1125769328@qq.com>
2025-02-14 17:24:34 +0800

    优化前端展示

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import joblib
 import os
-from model.predict import load_model_and_vectorizer,predict_text
+from model.predict import load_model_and_vectorizer, predict_text
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_directory)
@@ -15,12 +15,16 @@ model, vectorizer = load_model_and_vectorizer('model/GaussianNB.pkl', 'model/tfi
 # 定义主页路由
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    prediction = None
+    predictions = None
     result = ['正常文本', '弱口令', '令牌']
     if request.method == 'POST':
         input_text = request.form['text']
-        prediction = predict_text(input_text, model, vectorizer)
-    return render_template('index.html', prediction='{} = {}'.format(input_text,result[prediction]) if prediction is not None else None)
+        lines = input_text.split('\n')
+        predictions = {}
+        for line in lines:
+            prediction = predict_text(line.strip(), model, vectorizer)
+            predictions[line.strip()] = result[prediction]
+    return render_template('index.html', predictions=predictions)
 
 # 运行 Flask 应用
 if __name__ == '__main__':
